875c17f8ac3d75a1f793ecd39b2cb3f26628f112
philipxunwu <pwuxun@gmail.com>
2024-07-18 10:41:28 -0500

    Setting up raytracer project

---
+++ build/CMakeFiles/Raytracing.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Raytracing.dir/src/main.cpp.o"
+  "CMakeFiles/Raytracing.dir/src/main.cpp.o.d"
+  "Raytracing"
+  "Raytracing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Raytracing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
